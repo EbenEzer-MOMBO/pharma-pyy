19a9949dbb061024dfc9f289ef60934266322084
ebenezermombo@gmail.com <ebenezermombo@gmail.com>
2023-11-19 02:40:10 +0000

    Created Authentification

---
+++ server_code/Authentification.py
@@ -0,0 +1,9 @@
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def Connexion (email, passwd):
+  pass
