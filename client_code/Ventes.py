7f18d536dc5e7400152cb38fd223302a1ee4a050
ebenezermombo@gmail.com <ebenezermombo@gmail.com>
2023-11-23 12:07:38 +0000

    Created RowTemplate3; Edited settings, Ventes and ListeVentes

--- client_code/Ventes.py
+++ client_code/Ventes.py
@@ -12,35 +12,39 @@ class Ventes(VentesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.drop_down_2.items = clients
+    self.drop_client.items = clients
     produits = anvil.server.call('ListeProduitsDropdown')
-    self.drop_down_3.items = [libelle for libelle, prix in produits]
-    self.text_box_1.text = [prix for prix, prix in produits]
+    self.drop_prod.items = [libelle for libelle, prix in produits]
+    self.text_prix.text = [prix for prix, prix in produits]
 
-  def drop_down_3_change(self, **event_args):
+  def drop_prod_change(self, **event_args):
     produits = anvil.server.call('ListeProduitsDropdown')
-    selected_libelle = self.drop_down_3.selected_value  # Récupère le libellé sélectionné
+    selected_libelle = self.drop_prod.selected_value  # Récupère le libellé sélectionné
     # Recherche le prix correspondant au libellé sélectionné dans 'produits'
     selected_prix = next((prix for libelle, prix in produits if libelle == selected_libelle), None)
     if selected_prix is not None:
-        self.text_box_1.text = str(selected_prix)  # Assigner le prix au champ de texte
+        self.text_prix.text = str(selected_prix)  # Assigner le prix au champ de texte
     else:
-        self.text_box_1.text = ""  # Effacer le champ si aucun prix correspondant n'est trouvé
+        self.text_prix.text = ""  # Effacer le champ si aucun prix correspondant n'est trouvé
 
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
     pass
 
   def button_1_click(self, **event_args):
-    nom_complet = self.nom_complet.text
-    adresse = self.adresse.text
-    if nom_complet!='' and adresse!='':
-      anvil.server.call('AjoutClient', nom_complet, adresse)
+    nom_complet = self.drop_client.selected_value
+    produit = self.drop_prod.selected_value
+    prix = self.text_prix.text
+    qte = self.text_qte.text
+    if nom_complet!='' and produit!='' and prix!='' and qte!='':
+      anvil.server.call('AjouterVente', nom_complet, produit, prix, qte)
       #rafraichir
-      self.repeating_panel_1.items = anvil.server.call('ListeClients')
+      self.repeating_panel_1.items = anvil.server.call('ListeVentes')
       #effacer les champs
-      self.nom_complet.text = ''
-      self.adresse.text = ''
+      self.drop_client.text = ''
+      self.drop_prod.text = ''
+      self.text_prix.text = ''
+      self.text_qte.text = ''
     else:
       alert('Veuillez remplir tous les champs !')
 
--- server_code/ListeVentes.py
+++ server_code/ListeVentes.py
@@ -3,8 +3,28 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import datetime
+import random
+import string
 
 @anvil.server.callable
 def ListeProduitsDropdown():
     produits = app_tables.produit.search()
     return [(produit['libelle'], produit['prix']) for produit in produits]
+
+def generate_unique_id(length=4):
+    characters = string.ascii_letters + string.digits  # Chars possibles pour l'ID
+    unique_id = ''.join(random.choice(characters) for _ in range(length))
+    return unique_id
+
+@anvil.server.callable
+def ListeVentes():
+  return app_tables.vente.search()
+
+@anvil.server.callable
+def AjouterVente(nom, produit, prix, qte):
+  id = generate_unique_id()
+  delivre_le = datetime.date.today()
+  prix_numerique = float(prix) if isinstance(prix, str) else prix
+  total = prix_numerique * qte
+  app_tables.vente.add_row(id=id, nom_complet=nom, produit=produit, qte=qte, delivre_le=delivre_le, total=total)
