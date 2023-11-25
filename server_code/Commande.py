6eff164d41c4ecec1308e3e5aae75843d6739f76
ebenezermombo@gmail.com <ebenezermombo@gmail.com>
2023-11-25 22:42:39 +0000

    Created Commande

---
+++ server_code/Commande.py
@@ -0,0 +1,35 @@
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import datetime
+import random
+import string
+
+def generate_unique_id(length=4):
+    characters = string.ascii_letters + string.digits  # Chars possibles pour l'ID
+    unique_id = ''.join(random.choice(characters) for _ in range(length))
+    return unique_id
+
+@anvil.server.callable
+def ListeCommandesAttente():
+  return app_tables.commande_attente.search()
+
+@anvil.server.callable
+def ListeCommandesRecues():
+  return app_tables.commande_recues.search()
+
+@anvil.server.callable
+def AjouterCommande(produit, qte):
+  id = generate_unique_id()
+  ajoute_le = datetime.date.today()
+  app_tables.commande_attente.add_row(id=id, produit=produit, qte=qte, modifie_le=ajoute_le)
+
+@anvil.server.callable
+def ConfirmerCommande():
+  passs
+
+@anvil.server.callable
+def AnnulerCommande(commande):
+  commande.delete()
