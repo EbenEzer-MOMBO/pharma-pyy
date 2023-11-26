2154dc61a1a3bb7c0d48cf1f70c9592a00f64376
ebenezermombo@gmail.com <ebenezermombo@gmail.com>
2023-11-26 00:03:58 +0000

    Edited Authentification; Deleted Commande, Produits, ListeVentes and ListeClients

--- server_code/Authentification.py
+++ server_code/Authentification.py
@@ -3,6 +3,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import datetime
+import random
+import string
 
 
 @anvil.server.callable
@@ -18,3 +21,100 @@ def Connexion (email, passwd):
         return "Connexion réussie"
     else:
         return "Email ou mot de passe incorrect"
+
+def generate_unique_id(length=3):
+    characters = string.ascii_letters + string.digits  # Chars possibles pour l'ID
+    unique_id = ''.join(random.choice(characters) for _ in range(length))
+    return unique_id
+
+#Produit==========================
+
+@anvil.server.callable
+def ListeProduits():
+  return app_tables.produit.search()
+
+@anvil.server.callable
+def ModifierProduit(produit, libelle, qte, prix):
+  produit.update(libelle=libelle, qte=qte, prix=prix, date_ajout=datetime.date.today())
+
+@anvil.server.callable
+def AjouterProduit(libelle, qte, prix):
+    new_id = generate_unique_id(length=3)
+
+    # Obtenir la date actuelle
+    date_ajout = datetime.date.today()
+
+    # Ajouter une nouvelle ligne avec le nouvel identifiant et la date actuelle
+    app_tables.produit.add_row(id=new_id, libelle=libelle, qte=qte, prix=prix, date_ajout=date_ajout)
+
+@anvil.server.callable
+def SupprimerProduit(produit):
+  produit.delete()
+
+  
+#Vente==========================
+
+@anvil.server.callable
+def ListeProduitsDropdown():
+    produits = app_tables.produit.search()
+    return [(produit['libelle'], produit['prix']) for produit in produits]
+
+@anvil.server.callable
+def ListeVentes():
+  return app_tables.vente.search()
+
+@anvil.server.callable
+def AjouterVente(nom, produit, prix, qte):
+  id = generate_unique_id()
+  delivre_le = datetime.date.today()
+  prix_numerique = float(prix) if isinstance(prix, str) else prix
+  total = prix_numerique * qte
+  app_tables.vente.add_row(id=id, nom_complet=nom, produit=produit, qte=qte, delivre_le=delivre_le, total=total)
+
+
+#Commande==========================
+
+@anvil.server.callable
+def ListeCommandesAttente():
+  return app_tables.commande_attente.search()
+
+@anvil.server.callable
+def ListeCommandesRecues():
+  return app_tables.commande_recues.search()
+
+@anvil.server.callable
+def AjouterCommande(produit, qte):
+  id = generate_unique_id()
+  ajoute_le = datetime.date.today()
+  app_tables.commande_attente.add_row(id=id, produit=produit, qte=qte, modifie_le=ajoute_le)
+
+@anvil.server.callable
+def ConfirmerCommande(commande):
+  id = generate_unique_id()
+  recu_le = datetime.date.today()
+  app_tables.commande_recues.add_row(produit=commande['produit'], qte=commande['qte'], id=id, recu_le=recu_le)
+
+@anvil.server.callable
+def AnnulerCommande(commande):
+  commande.delete()
+
+
+#Client==========================
+
+@anvil.server.callable
+def ListeClients():
+  return app_tables.client.search()
+
+@anvil.server.callable
+def AjoutClient(nom, adresse):
+    new_id = generate_unique_id(length=3)
+    
+    app_tables.client.add_row(id=new_id, nom_complet=nom, adresse=adresse)
+
+@anvil.server.callable
+def ModifierClient(client, nom_complet, adresse):
+  client.update(nom_complet=nom_complet, adresse=adresse)
+
+@anvil.server.callable
+def SupprimerClient(client):
+  client.delete()
